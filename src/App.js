f41f44a9420f3c1ab5ec7af7c28be7e9786e7be3
ebenezer <e.kankam95@hotmail.com>
2021-06-21 10:51:19 -0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Switch>
+      <Route exact path='/'>
+        This is home
+      </Route>
+      <Route exact path='/starred'>
+        This is starred
+      </Route>
+      <Route>
+        This is a 404 page
+      </Route>
+    </Switch>
   );
 }
 
