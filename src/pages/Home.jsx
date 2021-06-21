3880f36c6db265a75f03b6881983c70b57736066
ebenezer <e.kankam95@hotmail.com>
2021-06-21 11:57:18 -0500

    get results from api and small refactor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,19 @@
 import { useState } from 'react';
 import MainPageLayout from "../components/MainPageLayout/MainPageLayout"
-
+import { apiGet } from '../misc/config';
 const Home = () => {
     const [searchInput, setSearchInput] = useState('');
+    const [result, setResult] = useState(null);
+
 
     const onInputChange = (e) => {
         setSearchInput(e.target.value)
     }
 
     // fetch api
-    const onSearch = async () => {
-        let API_URL = `https://api.tvmaze.com/search/shows?q=${searchInput}`;
-
-        const response = await fetch(API_URL);
-        const data = await response.json();
-        console.log(data);
+    const onSearch = () => {
+        apiGet(`/search/shows?q=${searchInput}`)
+            .then(data => setResult(data));
     }
 
     // grab search result by pressing enter button
@@ -22,10 +21,29 @@ const Home = () => {
         if (e.keyCode === 13) onSearch();
     }
 
+    // render results
+    const renderResult = () => {
+        if (result && result.length === 0) {
+            return <div>No results</div>
+        }
+        if (result && result.length > 0) {
+            return (
+                <div>
+                    {result.map(item => (
+                        <div key={item.show.id}>{item.show.name}</div>
+                    ))}
+                </div>
+            )
+        }
+
+        return null;
+    }
+
     return (
         <MainPageLayout>
             <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={searchInput} />
             <button type='button' onClick={onSearch}>Search</button>
+            {renderResult()}
         </MainPageLayout>
     )
 }
