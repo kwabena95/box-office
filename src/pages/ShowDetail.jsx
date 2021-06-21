11ea3572e5aaaea2bb9ea8d6071f8fe992dded3f
ebenezer <e.kankam95@hotmail.com>
2021-06-21 14:32:28 -0500

    added loading and error messages to show detail page

--- src/pages/ShowDetail.jsx
+++ src/pages/ShowDetail.jsx
@@ -7,15 +7,38 @@ const ShowDetail = () => {
     const { id } = useParams();
 
     const [show, setShow] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
 
+        let isMounted = true;
+
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
             .then(data => {
-                setShow(data)
+                if (isMounted) {
+                    setShow(data);
+                    setIsLoading(false);
+                }
+            })
+            .catch(err => {
+                if (isMounted) {
+                    setError(err.message);
+                    setIsLoading(false);
+                }
             });
 
+        return () => {
+            isMounted = false;
+        }
     }, [id]);
+
+    if (isLoading) {
+        return <div>loading...</div>
+    }
+    if (error) {
+        return <div>{error}</div>
+    }
 
     return (
         <div>
